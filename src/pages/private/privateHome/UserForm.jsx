1b5ec0c9f84e7578a1ae126d6d857b752b4e9fa0
KyleAktr <aktr.contact@gmail.com>
2025-02-12 11:57:09 +0100

    form infos added and style start

--- src/pages/private/privateHome/UserForm.jsx
+++ src/pages/private/privateHome/UserForm.jsx
@@ -6,8 +6,20 @@ import { doc, setDoc, getDoc } from "firebase/firestore";
 
 const UserForm = () => {
   const { currentUser } = useContext(UserContext);
-  const [age, setAge] = useState(0);
+  const [validation, setValidation] = useState("");
+  const [nom, setNom] = useState("");
+  const [dicipline, setDicipline] = useState([
+    "musique",
+    "art du spéctacle",
+    "peinture",
+  ]);
+  const [details, setDetails] = useState("");
   const [city, setCity] = useState("");
+  const [bio, setBio] = useState("");
+  const [email, setEmail] = useState("");
+  const [website, setWebsite] = useState("");
+
+  const [age, setAge] = useState(0);
 
   useEffect(() => {
     if (currentUser) {
@@ -16,8 +28,14 @@ const UserForm = () => {
         const docSnap = await getDoc(userRef);
 
         if (docSnap.exists()) {
-          setAge(docSnap.data().age);
+          setNom(docSnap.data().nom);
+          setDetails(docSnap.data().details);
           setCity(docSnap.data().city);
+          setBio(docSnap.data().bio);
+          setEmail(docSnap.data().email);
+          setWebsite(docSnap.data().website);
+
+          setAge(docSnap.data().age);
         }
       };
 
@@ -28,7 +46,11 @@ const UserForm = () => {
   const handleSave = async () => {
     if (currentUser) {
       const userRef = doc(db, "utilisateurs", currentUser.uid);
-      await setDoc(userRef, { age, city }, { merge: true });
+      await setDoc(
+        userRef,
+        { age, city, nom, details, bio, email, website },
+        { merge: true }
+      );
       alert("Profil mis à jour !");
     }
   };
@@ -36,21 +58,84 @@ const UserForm = () => {
   return (
     <div>
       <NavUser />
-      <label>Âge :</label>
-      <input
-        type="number"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
 
-      <label>Ville :</label>
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
+      <h1>
+        Complétez votre profil pour être découvert et pour vous connecter avec
+        d'autres artistes.
+      </h1>
+      <div className="form-user">
+        <div className="user-infos">
+          <h2>Informations générales</h2>
+          <label>
+            Nom :
+            <input
+              type="text"
+              value={nom}
+              onChange={(e) => setNom(e.target.value)}
+            />
+          </label>
+          <label>
+            Dicipline(s) :
+            <input />
+          </label>
 
-      <button onClick={handleSave}>Enregistrer</button>
+          <label>
+            Détails spécifiques selon la dicipline artistique :
+            <input
+              type="text"
+              value={details}
+              onChange={(e) => setDetails(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Ville :
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Âge :
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Bio :
+            <input
+              type="text"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div className="user-contact">
+          <h2>Coordonnées et réseaux</h2>
+          <label>E-mail</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Site web/portfolio</label>
+          <input
+            type="text"
+            value={website}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
+
+        <p className="validation">{validation}</p>
+        <button onClick={handleSave}>Enregistrer</button>
+      </div>
     </div>
   );
 };
