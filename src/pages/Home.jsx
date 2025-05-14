09c10b86feeedbcbcbbc4e5622ae22a167b72ceb
KyleAktr <aktr.contact@gmail.com>
2025-05-14 10:52:18 +0200

    text-description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import NavUser from "../components/NavUser";
 import Nav from "../components/Nav";
 import "../styles/pages/_home.scss";
 import ThemeToggle from "../components/ThemeToggle";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { currentUser } = useContext(UserContext);
@@ -11,7 +12,7 @@ const Home = () => {
   return (
     <div className="home-page">
       {currentUser ? <NavUser /> : <Nav />}
-      
+
       <div className="hero-section">
         <div className="logo">
           <h1>artis</h1>
@@ -24,6 +25,30 @@ const Home = () => {
             <span className="arrow">&#8964;</span>
             <span className="arrow">&#8964;</span>
           </div>
+        </div>
+      </div>
+      <div className="home-description">
+        <div className="home-description-text">
+          <p>
+            <strong> ARTIS </strong> est un site internet qui permet aux
+            artistes de différents domaines (musique, graphisme, danse, cinéma,
+            mode, etc.) de collaborer ensemble. <br />
+          </p>
+          <p>
+            Le principe est simple : un artiste publie une annonce pour son
+            projet en indiquant le type de profil qu’il recherche. Par exemple,
+            un rappeur qui cherche un vidéaste pour réaliser un clip. Les
+            artistes intéressés par l’annonce peuvent répondre et proposer leur
+            aide. Une fois mis en contact, ils peuvent travailler ensemble pour
+            concrétiser le projet.
+          </p>
+          <br />
+          <p>
+            <strong> ARTIS </strong> facilite ainsi les rencontres créatives et
+            les collaborations entre artistes.
+          </p>
+
+          <Link className="colorbtn">Collaborer</Link>
         </div>
       </div>
       <ThemeToggle />
